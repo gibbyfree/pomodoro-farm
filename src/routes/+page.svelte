29d923e3766925ffbe692b5ef25a667e2e5b7458
Gibby Free <gib@gibby.dev>
2024-11-10 23:50:01 -0800

    progress bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <script>
 	import '@fortawesome/fontawesome-free/css/all.min.css';
-
-	let now = $state(new Date());
-	let end = $state(new Date());
-    let done = $state(false);
-    let disabled = $state(false);
+    import { ProgressBar } from '@skeletonlabs/skeleton';
 
     // Normal pomodoro: 25mins. For dev, 1.
     const timerMins = 1;
+    const maxRemaining = timerMins * 60 * 1000;
+
+	let now = $state(new Date());
+	let end = $state(new Date());
+    let remaining = $state(0);
+    
+    let remainingPct = $derived.by(() => {
+        return Math.floor((remaining / maxRemaining) * 100);
+    });
+
+    let done = $state(false);
+    let disabled = $state(false);
 
     $effect.root(() => {
         const storedEnd = localStorage.getItem('end');
@@ -21,11 +29,11 @@
 		const interval = setInterval(() => {
 			now = new Date();
             if (now >= end) {
-                clearInterval(interval);
                 done = true;
                 disabled = false;
                 localStorage.removeItem('end');
             }
+            remaining = end.getTime() - now.getTime();
 		}, 500);
 
 		return () => clearInterval(interval);
@@ -37,6 +45,7 @@
         } else {
             disabled = true;
             done = false;
+
             let newEnd = new Date(end.setMinutes(now.getMinutes() + timerMins));
             localStorage.setItem('end', newEnd.toISOString());
             return newEnd;
@@ -48,7 +57,10 @@
 	<header class="card-header">Pomo Farm</header>
 	<section class="p-4">Current time: {now.toUTCString()}</section>
     {#if disabled}
-	    <footer class="card-footer">End time: {end.toUTCString()}</footer>
+	    <footer class="card-footer">
+            End time: {end.toUTCString()}
+            <ProgressBar value={remainingPct} max={100} />
+        </footer>
     {/if}
     {#if done}
         <footer class="card-footer">Done!</footer>
@@ -59,7 +71,9 @@
     disabled={disabled}
 	type="button"
 	class="btn variant-filled btn-lg"
-	onclick={() => end = setTimer()}>
+	onclick={() => setTimer()}>
 	<span> <i class="fa-regular fa-tractor"> </i></span>
 	<span>Set Timer</span>
 </button>
+
+{@debug remaining}
