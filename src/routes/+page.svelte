eee0ea5651a7fbbce811b4643a2cbbaffb8b91b1
Gibby Free <gib@gibby.dev>
2024-11-10 22:03:24 -0800

    init skeleton installation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { tick } from 'svelte';
+
+    let now = $state(new Date().toISOString());
+
+    $effect(() => {
+        const interval = setInterval(() => {
+            now = new Date().toISOString();
+        }, 1000);
+
+        return () => clearInterval(interval);
+    });
+
+    // Normal pomodoro: 25mins. For dev, 5s.
+
+</script>
+
+<div class="card p-4">Current time: {now}</div>
+
