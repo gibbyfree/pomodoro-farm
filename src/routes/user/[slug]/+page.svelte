33320cb11693d18359b6935583a63d2df8b0c61c
Gibby Free <gib@gibby.dev>
2024-11-29 20:24:48 -0800

    copy paste framework

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -1,26 +1,73 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import type { ModalComponent, ModalSettings, PopupSettings } from '@skeletonlabs/skeleton';
 	import { PUBLIC_SUPABASE_URL } from '$env/static/public';
-	import { Avatar } from '@skeletonlabs/skeleton';
+	import { Avatar, getModalStore, popup } from '@skeletonlabs/skeleton';
+	import { cUser } from '$lib/state/user.svelte';
+	import FormModal from '$lib/components/FormModal.svelte';
+	import { supabase } from '$lib/supabase';
 
 	let { data }: { data: PageData } = $props();
 	let user = data.user ?? { id: 'loading', username: 'loading' };
 
 	const avatarUrl = `${PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/bluebear2.jpg`;
+
+	const modalStore = getModalStore();
+	const popupHover: PopupSettings = {
+		event: 'hover',
+		target: 'popupHover',
+		placement: 'top'
+	};
+
+	function usernameForm(userId: any): void {
+		const c: ModalComponent = { ref: FormModal };
+		const modal: ModalSettings = {
+			type: 'component',
+			component: c,
+			title: 'Howdy, stranger! What should we call you?',
+			body: '',
+			response: (r) => {
+				updateUserDetails(userId, r);
+			}
+		};
+		modalStore.trigger(modal);
+	}
+
+	async function updateUserDetails(userId: any, details: any): Promise<void> {
+		const { data, error } = await supabase.from('users').update(details).eq('id', userId);
+		if (error) {
+			console.error('Error updating user details:', error);
+		} else {
+			console.log('User details updated:', data);
+		}
+	}
 </script>
 
 <div class="grid h-full w-full grid-cols-10 grid-rows-5 gap-4">
 	<div class="col-span-3 row-span-3 rounded-xl p-4">
 		<Avatar src={avatarUrl} alt="avatar" width="w-full" rounded="rounded-md" />
-		<div class="variant-filled-secondary btn-group my-4 w-full justify-center">
-			<button>Add</button>
-			<button>Trade</button>
-			<button>DM</button>
-		</div>
+		{#if user.id !== cUser.id}
+			<div class="variant-filled-secondary btn-group my-4 w-full justify-center">
+				<button>Add</button>
+				<button>Trade</button>
+				<button>DM</button>
+			</div>
+		{:else}
+			<div class="my-4 w-full justify-center">
+				<button
+					type="buton"
+					aria-label="Edit"
+					class="variant-filled-secondary btn [&>*]:pointer-events-none"
+					use:popup={popupHover}
+				>
+					<i class="fa-solid fa-pencil"></i>
+				</button>
+			</div>
+		{/if}
 	</div>
 	<div class="col-span-7 col-start-4 row-span-3 rounded-xl">
 		<h2 class="h2 mb-2 text-3xl font-extrabold md:text-5xl">
-			{user.username}
+			🎃 {user.username}
 		</h2>
 		<div class="card mb-2 h-3/4 p-4">
 			<section class="text-l font-medium leading-relaxed">i shrimply love to farm</section>
@@ -29,4 +76,9 @@
 			<section class="text-l font-medium leading-relaxed">milestones</section>
 		</div>
 	</div>
+</div>
+
+<div class="card variant-filled-secondary p-4" data-popup="popupHover">
+	<p>Logout</p>
+	<div class="variant-filled-secondary arrow"></div>
 </div>
